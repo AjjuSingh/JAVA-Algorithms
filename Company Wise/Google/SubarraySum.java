37e7ce5bc2ae945d8c346cb15452c7b029d527f7
AjjuSingh <ajaykumar.kumar900@gmail.com>
2020-10-26 13:01:07 +0530

    Company wise questions

---
+++ Company Wise/Google/SubarraySum.java
@@ -0,0 +1,12 @@
+/**
+ * https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0
+ */
+
+import java.util.*;
+import java.lang.*;
+
+public class SubarraySum {
+    public static void main(String[] args) {
+        System.out.println("Subarray Problem");
+    }
+}   
